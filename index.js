29bfda66ea4e24f79e83a3fae7568bfb70ece483
Karl Beecken <karl@beecken.berlin>
2020-06-21 12:39:26 +0200

    🌐 add axios

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const Twitter = require('twitter-lite')
+const axios = require('axios')
 
 const client = new Twitter({
     subdomain: "api", // "api" is the default (change for other subdomains)
@@ -10,18 +11,17 @@ const client = new Twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET // from your User (oauth_token_secret)
   })
 
-client
-  .get("account/verify_credentials")
-  .then(results => {
-    console.log("results", results)
+axios.get('http://www.hqcasanova.com/co2/')
+  .then(function (rsp) {
+    console.log("current:", rsp.data)
+    client
+    .post("account/update_profile", {
+      description: process.env.DESCRIPTION + " " + rsp.data
+    })
+    .then(results => {
+      console.log("new bio:", results.description)
+    })
+    .catch(console.error)
   })
   .catch(console.error)
 
-client
-  .post("account/update_profile", {
-      description: process.env.DESCRIPTION
-  })
-  .then(results => {
-    console.log("results", results)
-  })
-  .catch(console.error)
